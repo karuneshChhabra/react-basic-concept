e3e05cde026216ce928494300222558dc80b3312
karunesc <karunesh.chhabra@nokia.com>
2021-09-04 12:47:46 +0530

    commit first lifecycle method componentDidUpdate

--- lifecycle-method-2/src/App.js
+++ lifecycle-method-2/src/App.js
@@ -8,7 +8,8 @@ class App extends React.Component {
     // used for initialize the state
     console.log("constructor call");
 
-    this.state={data:null};
+    this.state={data:null,
+    activate:null};
   }
 
   componentWillMount(){
@@ -21,11 +22,27 @@ class App extends React.Component {
     console.log("called after render method");
   }
 
+  
+  activateState = ()=>{
+    console.log("called");
+     this.setState({activate:'active'});
+  }
+
+  componentDidUpdate(prevProps,prevState){
+    console.log("componentDidUpdate is called");
+    console.log(prevState);
+    if(prevState.activate !==this.state.activate){
+      console.log("state changed");
+    
+    }
+  }
+
   render(){
     console.log("render call");
   return (
     <div className="App">
-      Life Cycle methods 
+      
+      <button onClick={this.activateState}>Activate state</button>
     </div>
   )};
 }
