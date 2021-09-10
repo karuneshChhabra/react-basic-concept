fa301f352bcfc6fc36a4391827ef314f7ec4584b
karunesc <karunesh.chhabra@nokia.com>
2021-09-10 17:47:55 +0530

    initial promises

---
+++ promises/src/App.js
@@ -0,0 +1,32 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  function getCurrentTime(onSuccess, onFail) {
+    // Get the current 'global' time from an API
+    return setTimeout(function () {
+      // randomly decide if the date is retrieved or not
+      var randomValue = Math.random();
+      var didSucceed = randomValue >= 0.5;
+      if (didSucceed) {
+        var currentTime = new Date();
+        onSuccess(currentTime);
+      } else {
+        onFail("Unknown error");
+      }
+    }, 2000);
+  }
+
+  getCurrentTime(
+    function (currentTime) {
+      console.log("The current time is: " + currentTime);
+    },
+    function (error) {
+      console.log("There was an error fetching the time");
+    }
+  );
+
+  return <div className="App"></div>;
+}
+
+export default App;
