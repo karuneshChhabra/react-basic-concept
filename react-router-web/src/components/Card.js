65aa95ba890f1f86da2173d2e68d88fff3884976
karunesc <karunesh.chhabra@nokia.com>
2021-09-15 15:59:38 +0530

    basic routing

---
+++ react-router-web/src/components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Card.css';
+import juice from './juice.jpg';
+
+const Card = (props) =>  (
+  <div className={props.card.animation}>
+    <div className="front">
+      <img src={juice} alt="Avatar" className="card-image" />
+      <div className="container">
+        <h3>{props.card.title}</h3>
+        <h3><span className="price"> ${props.card.price}</span></h3> 
+        <p>{props.card.description}</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default Card;
---
+++ react-router-web/src/components/Vitamin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Vitamin() {
+    return (
+        <div>
+            <h1>Vitamin juice</h1>
+        </div>
+    )
+}
+
+export default Vitamin
