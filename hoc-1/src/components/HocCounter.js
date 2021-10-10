50012d129564b36ee7a33bb409ec772064b49537
karunesc <karunesh.chhabra@nokia.com>
2021-10-10 15:05:52 +0530

    hoc formatting

--- hoc-1/src/components/HocCounter.js
+++ hoc-1/src/components/HocCounter.js
@@ -1,32 +1,28 @@
-import React, {useState} from 'react';
+import React from "react";
 
-const  hocCounter =  WrappedComponent => {
-
-    return class HocCounter extends React.Component{
-
-        constructor(props){
-            super(props);
-            console.log(props);
-            this.state={
-                counter:0
-            }
-        }
-    
-     incrementValue= () =>{
-         const {counter} ={...this.state};
-         console.log(counter);
-        this.setState({counter:counter+1});
-       
+const hocCounter = (WrappedComponent) => {
+  return class HocCounter extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        counter: 0,
+      };
     }
-    render(){
-      return ( 
-      <WrappedComponent counter={this.state.counter} incrementValue={this.incrementValue} {...this.props}/>
-      )
-    }  
-   
-   }
 
-}
-
+    incrementValue = () => {
+      const { counter } = { ...this.state };
+      this.setState({ counter: counter + 1 });
+    };
+    render() {
+      return (
+        <WrappedComponent
+          counter={this.state.counter}
+          incrementValue={this.incrementValue}
+          {...this.props}
+        />
+      );
+    }
+  };
+};
 
 export default hocCounter;
